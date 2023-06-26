bcada621df7ce84ae56e330e3177bd0d9e6a3154
JChimbo <jchimbo@oswego.edu>
2023-06-26 13:23:07 -0400

    Changed imports

--- src/main/java/oswego/webservices/Homework7/controller/BookResource.java
+++ src/main/java/oswego/webservices/Homework7/controller/BookResource.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import oswego.webservices.Homework7.api.Book;
 import java.io.IOException;
 
