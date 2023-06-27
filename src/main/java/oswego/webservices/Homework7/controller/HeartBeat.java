982a4b961f19e4d8c005780c9a44d11a501d56ff
JChimbo <jchimbo@oswego.edu>
2023-06-27 11:26:49 -0400

    Added Logging Support for HeartBeat.java

--- src/main/java/oswego/webservices/Homework7/controller/HeartBeat.java
+++ src/main/java/oswego/webservices/Homework7/controller/HeartBeat.java
@@ -1,5 +1,7 @@
 package oswego.webservices.Homework7.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/heartbeat")
 public class HeartBeat {
+    Logger logger = LoggerFactory.getLogger(HeartBeat.class);
+
 
     @GetMapping
     public Boolean isAuth(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return !(authentication instanceof AnonymousAuthenticationToken);
+        Boolean auth = !(authentication instanceof AnonymousAuthenticationToken);
+        logger.info("User has auth: " + auth);
+        return auth;
     }
 }
