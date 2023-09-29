530f0188002fe5bd9ad31f521439955e4442617d
JChimbo <jchimbo@oswego.edu>
2023-09-29 19:07:17 -0400

    Added grow animation on bookshelf for interactivity.

--- frontend/src/components/bookshelf.js
+++ frontend/src/components/bookshelf.js
@@ -2,23 +2,24 @@ import Card from 'react-bootstrap/Card';
 import axios from "axios";
 import "../App.scss";
 import React, {useEffect, useState} from "react";
-import {Accordion, Col, Row} from "react-bootstrap";
+import {Accordion, Col, Placeholder, Row} from "react-bootstrap";
 import {EndNav} from "./endNav";
 import Logo from "./logo";
 
 
 function BookShelf() {
-    const [listData,setListData]=useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const [listData, setListData] = useState([]);
     useEffect(
-        ()=>{
+        () => {
             axios.get("/booklist")
-                .then(res =>
-                {
+                .then(res => {
                     console.log(res.data);
                     setListData(res.data);
+                    setIsLoading(false);
                 })
                 .catch(error => console.log(error));
-        },[]
+        }, []
     )
     return (
         <div id="topBar">
@@ -33,32 +34,66 @@ function BookShelf() {
             </div>
 
             <div className="container overflow-hidden text-center p-5" id="bookShelf">
-              <Row className="g-1" id="Shelf">
-                  {listData.map((data, id) =>
-                  {
-                      return (
-                          <Col key={id}>
-                              <Card style={{ width: '18rem' }}>
-                                  <Card.Img variant="top" src={data.cover} />
-                                  <Card.Body>
-                                      <Accordion>
-                                          <Accordion.Item eventKey="0">
-                                              <Accordion.Header>{data.title}</Accordion.Header>
-                                              <Accordion.Body>
-                                                  {data.description}
-                                                  {<br></br>}
-                                                  {"By " + data.author}
-                                              </Accordion.Body>
-                                          </Accordion.Item>
-                                      </Accordion>
-                                  </Card.Body>
-                              </Card>
-                          </Col>
-                      )} )}
-              </Row>
-          </div>
+                <Row className="g-1" id="Shelf">
+
+                    {isLoading ?
+                        <CardLoading/>
+                        : listData.map((data, id) => {
+                                return (
+                                    <Col key={id}>
+                                        <Card style={{width: '18rem'}}>
+                                            <Card.Img variant="top" src={data.cover} width={286} height={360}/>
+                                            <Card.Body>
+                                                <Accordion>
+                                                    <Accordion.Item eventKey="0">
+                                                        <Accordion.Header>{data.title}</Accordion.Header>
+                                                        <Accordion.Body>
+                                                            {data.description}
+                                                            {<br></br>}
+                                                            {"By " + data.author}
+                                                        </Accordion.Body>
+                                                    </Accordion.Item>
+                                                </Accordion>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+
+                                )
+                            }
+                        )
+                    }
+                </Row>
+            </div>
         </div>
     );
+}
+
+
+function CardLoading() {
+    let arr = [];
+    for(let x = 0; x < 4; x++){
+        arr.push(<Card style={{width: '18rem'}}>
+            <Card.Img variant="top"  src="" style={{backgroundColor: "#8D5A16" }} width={286} height={360} />
+            <Card.Body>
+                <Accordion>
+                    <Accordion.Item eventKey="0">
+                        <Placeholder as={Accordion.Header} animation="glow">
+                            <Placeholder xs={6}/>
+                        </Placeholder>
+                        <Placeholder as={Accordion.Body} animation="glow">
+                            <Placeholder xs={7}/> <Placeholder xs={4}/> <Placeholder xs={4}/>{' '}
+                            <Placeholder xs={6}/> <Placeholder xs={8}/>
+                        </Placeholder>
+                    </Accordion.Item>
+                </Accordion>
+            </Card.Body>
+        </Card>)
+    }
+    return (
+        <>
+            {arr.values()}
+        </>
+    )
 }
 
 export default BookShelf;
