2381a33c98af73832a36e83bfdb621ccb22c4900
JChimbo <jchimbo@oswego.edu>
2023-11-05 12:59:49 -0500

    Removed older version test

--- src/test/java/site/jeremichimbo/BookSocketTest.java
+++ src/test/java/site/jeremichimbo/BookSocketTest.java
@@ -15,8 +15,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class BookSocketTest {
     String socket;
     URL OPEN_LIBRARY_API = new URL("https://openlibrary.org/isbn/9780451526533");
-    URL GOOGLE_API = new URL("https://www.googleapis.com/books/v1/volumes?q="+"9780451526533" +"&key=AIzaSyAUj62lGbMQGZYEOWc6vdLCDvcjpSDCl9I");
-
 
     @Ignore
     @Test
@@ -26,21 +24,11 @@ public class BookSocketTest {
 
     }
     @Test
-    public void googleBooksConnectionTest() throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) GOOGLE_API.openConnection();
-        assertEquals(connection.getResponseCode(), 200);
-
-    }
-    @Test
     public void openLibraryGetBookTest(){
         socket = new BookSocket(OPEN_LIBRARY_API).getJsonString();
         assertTrue(socket.contains("The adventures of Tom Sawyer"));
     }
-    @Test
-    public void googleBooksGetBookTest(){
-         socket = new BookSocket(GOOGLE_API).getJsonString();
-        assertTrue(socket.contains("The Adventures of Tom Sawyer"));
-    }
+
 
     public BookSocketTest() throws MalformedURLException {
     }
